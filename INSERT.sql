37dc8a408dd515989992a35c24bbe43b9c4fd286
Ярослав <spaceseenon@gmail.com>
2025-12-16 05:29:49 +0300

    Update INSERT.sql

--- INSERT.sql
+++ INSERT.sql
@@ -1,39 +1,37 @@
 
 -- Добавление исполнителей
-INSERT INTO musicians(id, nickname) VALUES
-(1, 'Исполнитель1'),
-(2, 'Исполнитель2'),
-(3, 'Исполнитель3'),
-(4, 'Исполнитель4');
-
+INSERT INTO Artists(id, nickname) VALUES
+(1, 'Artist1'),
+(2, 'Artist2'),
+(3, 'Artist3'),
+(4, 'Artist4');
 -- Добавление жанров
 INSERT INTO music_ganre(id, ganre_name) VALUES
-(1, 'Поп'),
-(2, 'Рок'),
-(3, 'Хип-хоп'),
-(4, 'Электронная музыка');
+(1, 'Pop'),
+(2, 'Rock'),
+(3, 'Hip-hop'),
+(4, 'Electronic music');
 
 -- Добавление альбомов
-INSERT INTO альбомы (id, название, год) VALUES
-(1, 'Альбом1', 2019),
-(2, 'Альбом2', 2020),
-(3, 'Альбом3', 2021);
+INSERT INTO albums (id, title, year) VALUES
+(1, 'Album1', 2019),
+(2, 'Album2', 2020),
+(3, 'Album3', 2021);
 
 -- Добавление треков
 INSERT INTO songs(song_id, song_name, duration, album_id) VALUES
-(1, 'Трек1', '3:30', 1),
-(2, 'Трек2', '4:15', 1),
-(3, 'Трек3', '2:45', 2),
-(4, 'Трек4', '3:05', 2),
-(5, 'Трек5', '4:55', 3),
-(6, 'Трек6', '5:10', 3);
-
+(1, 'Track1', '3:30', 1),
+(2, 'Track2', '4:15', 1),
+(3, 'Track3', '2:45', 2),
+(4, 'Track4', '3:05', 2),
+(5, 'Track5', '4:55', 3),
+(6, 'Track6', '5:10', 3);
 -- Добавление сборников
 INSERT INTO collection(collection_id, collection_name, release_year) VALUES
-(1, 'Сборник1', 2018),
-(2, 'Сборник2', 2019),
-(3, 'Сборник3', 2020),
-(4, 'Сборник4', 2021);
+(1, 'Compilation1', 2018),
+(2, 'Compilation2', 2019),
+(3, 'Compilation3', 2020),
+(4, 'Compilation4', 2021);
 
 -- Связь исполнителей и жанров
 INSERT INTO musicians_жанр (musicians_id, ganre_id) VALUES
@@ -52,7 +50,7 @@ INSERT INTO musicians_album (musicians_id, album_id) VALUES
 (4, 1), -- Исполнитель4 - Альбом1
 
 -- Связь сборников и треков
-INSERT INTO songscollection(collection_id, song_id) VALUES
+INSERT INTO songs_collection(collection_id, song_id) VALUES
 (1, 1), -- Сборник1 - Трек1
 (1, 2), -- Сборник1 - Трек2
 (2, 3), -- Сборник2 - Трек3
