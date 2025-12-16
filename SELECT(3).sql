166a171f470574e19c930d53069db3e113b00cb6
Ярослав <spaceseenon@gmail.com>
2025-12-16 05:44:12 +0300

    Update SELECT(3).sql

--- SELECT(3).sql
+++ SELECT(3).sql
@@ -3,42 +3,43 @@
 
 --1. Количество исполнителей в каждом жанре.
 
-SELECT ж.название AS жанр, COUNT(исполнитель.id) AS количество_исполнителей
-FROM жанры ж
-JOIN исполнитель_жанр еж ON ж.id = еж.жанр_id
-JOIN исполнители исп ON еж.исполнитель_id = исп.id
-GROUP BY ж.название;
+SELECT g.name AS genre, COUNT(artist.id) AS artist_count
+FROM genres g
+JOIN artist_genre ag ON g.id = ag.genre_id
+JOIN musicians artist ON ag.musicians_id = artist.id
+GROUP BY g.name;
 
 --2. Количество треков, вошедших в альбомы 2019–2020 годов.
 
-SELECT альб.год, COUNT(т.id) AS количество_треков
-FROM альбомы альб
-JOIN треки т ON альб.id = т.альбом_id
-WHERE альб.год BETWEEN 2019 AND 2020
-GROUP BY альб.год;
+SELECT alb.year, COUNT(t.id) AS track_count
+FROM albums alb
+JOIN songs t ON alb.id = t.album_id
+WHERE alb.year BETWEEN 2019 AND 2020
+GROUP BY alb.year;
 
 --3. Средняя продолжительность треков по каждому альбому.
 
-SELECT альб.название AS альбом, AVG(TIME_TO_SEC(т.продолжительность)) / 60 AS средняя_продолжительность_минут
-FROM альбомы альб
-JOIN треки т ON альб.id = т.альбом_id
-GROUP BY альб.название;
+SELECT alb.title AS album, AVG(TIME_TO_SEC(t.duration)) / 60 AS average_duration_minutes
+FROM albums alb
+JOIN songs t ON alb.id = t.album_id
+GROUP BY alb.title;
 
 --4. Все исполнители, которые не выпустили альбомы в 2020 году.
 
-SELECT исп.имя
-FROM исполнители исп
-WHERE исп.id NOT IN (SELECT исполнителей.id
-FROM исполнитель_альбом
-JOIN альбомы альб ON исполнитель_альбом.альбом_id = альб.id
-WHERE альб.год = 2020);
-
+SELECT artist.name
+FROM musicians artist
+WHERE artist.id NOT IN (
+SELECT ea.musicians_id
+FROM musicians_album ea
+JOIN albums alb ON ea.album_id = alb.id
+WHERE alb.year = 2020
+);
 --5. Названия сборников, в которых присутствует конкретный исполнитель
 
-SELECT сб.название
-FROM сборники сб
-JOIN сборник_трек ст ON сб.id = ст.сборник_id
-JOIN треки т ON ст.трек_id = т.id
-JOIN исполнитель_альбом еа ON т.альбом_id = еа.альбом_id
-JOIN исполнители исп ON еа.исполнитель_id = исп.id
-WHERE исп.имя = 'Исполнитель1';
+SELECT coll.title
+FROM collection coll
+JOIN songs_collection sc ON coll.collection_id = sc.collection_id
+JOIN songs t ON sc.song_id = t.song_id
+JOIN musicians_album ea ON t.album_id = ea.album_id
+JOIN musicians artist ON ea.musicians_id = artist.id
+WHERE artist.name = 'Artist1';
