3ea22d6f5d0579b71e48e370deed41998afdec27
Ярослав <spaceseenon@gmail.com>
2025-12-16 05:18:33 +0300

    Update SELECT.sql

--- SELECT.sql
+++ SELECT.sql
@@ -1,26 +1,25 @@
 -- 1. Название и продолжительность самого длительного трека
-SELECT название, продолжительность
-FROM треки
-ORDER BY продолжительность DESC
+SELECT title, duration
+FROM tracks
+ORDER BY duration DESC
 LIMIT 1;
 
 -- 2. Название треков, продолжительность которых не менее 3,5 минут
-SELECT название
-FROM треки
-WHERE продолжительность >= '00:03:30';
+SELECT title
+FROM tracks
+WHERE duration >= '00:03:30';
 
 -- 3. Названия сборников, вышедших в период с 2018 по 2020 год включительно
-SELECT название
-FROM сборники
-WHERE год BETWEEN 2018 AND 2020;
+SELECT title
+FROM compilations
+WHERE year BETWEEN 2018 AND 2020;
 
 -- 4. Исполнители, чьё имя состоит из одного слова
-SELECT имя
-FROM исполнители
-WHERE имя NOT LIKE '% %';
+SELECT name
+FROM artists
+WHERE name NOT LIKE '% %';
 
 -- 5. Название треков, которые содержат слово «мой» или «my»
-SELECT название
-FROM треки
-WHERE название LIKE '%мой%' OR название LIKE '%my%';
-
+SELECT title
+FROM tracks
+WHERE title LIKE '%мой%' OR title LIKE '%my%';
